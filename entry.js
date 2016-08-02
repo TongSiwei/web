2585f2056491507724814163e230b45654a169bf
tongsiwei <tongsiwei@zbj.com>
2016-08-02 14:27:55 +0800

    server start

--- entry.js
+++ entry.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+	res.sendfile('index.html');
+
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
+    console.log('server start at localhost:3000');
 });
